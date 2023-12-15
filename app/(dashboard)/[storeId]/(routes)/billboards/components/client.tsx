d6b92c409680181aaaba6c03674c3d39d1d25b8b
Rushi <rushikeshshelar.cs@gmail.com>
2023-12-16 00:52:52 +0530

    Adding Billboard & its Components

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+export const BillboardClient = () => {
+    const router = useRouter();
+    const params = useParams();
+
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading 
+                    title="Billboards (0)"
+                    description="Manage your billboards here."
+                />
+                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+        </>
+    )
+};
