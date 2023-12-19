ae064b3682b0802a980b1d77daad4038306cb584
Rushi <rushikeshshelar.cs@gmail.com>
2023-12-20 02:25:37 +0530

    API routes for Sizes

---
+++ app/api/[storeId]/sizes/route.ts
@@ -0,0 +1,71 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+    req: Request,
+    { params }: { params: { storeId: string } }
+){
+    try {
+        const { userId } = auth();
+
+        const body = await req.json();
+
+        const { name, value} = body;
+
+        if(!userId) return new NextResponse("Unauthenticated", { status: 401 });
+
+        if(!name) return new NextResponse("Name is Required", { status: 400 });
+
+        if(!value) return new NextResponse("Value is Required", { status: 400 });
+
+        if(!params.storeId) return new NextResponse("Store Id is Required", { status: 400 });
+
+        const storeByUserId = await prismadb.store.findFirst({
+            where:{
+                id: params.storeId,
+                userId
+            }
+        });
+
+        if(!storeByUserId) return new NextResponse("Unauthorized", { status: 403 });
+        
+        const size= await prismadb.size.create({
+            data:{
+                name,
+                value,
+                storeId: params.storeId
+            }
+        });
+
+        return NextResponse.json(name);
+
+    } catch (error) {
+        console.log('SIZES_POST', error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
+
+
+export async function GET(
+    req: Request,
+    { params }: { params: { storeId: string } }
+){
+    try {
+       
+        if(!params.storeId) return new NextResponse("Store Id is Required", { status: 400 });
+        
+        const sizes = await prismadb.size.findMany({
+            where:{
+                storeId: params.storeId,
+            }
+        });
+
+        return NextResponse.json(sizes);
+
+    } catch (error) {
+        console.log('SIZES_GET', error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
