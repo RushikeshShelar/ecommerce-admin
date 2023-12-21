fb67d15cd3986b7f6a1e8cfff5c13ab73839557f
Rushi <rushikeshshelar.cs@gmail.com>
2023-12-22 03:07:51 +0530

    Added id to Search Input to Fix Error

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -54,6 +54,7 @@ export function DataTable<TData, TValue>({
         <div>
             <div className="flex items-center py-4">
                 <Input
+                    id="search"
                     placeholder="Search"
                     value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
                     onChange={(event) =>
