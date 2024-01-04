1969953c5428c55f3ea073f2ffe728d151416859
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-04 21:34:59 +0530

    Added Dark Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToastProvider } from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
+
 
 import './globals.css'
 
@@ -23,10 +25,16 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToastProvider />
-          <ModalProvider />
-          {children}
-          </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          >
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
     const { userId} = auth();
@@ -27,6 +28,7 @@ const Navbar = async () => {
                 <MainNav className="mx-6"/>
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl="/" />
+                    <ModeToggle />
                 </div>
             </div>
         </div>
