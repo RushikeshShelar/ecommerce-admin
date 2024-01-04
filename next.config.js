5e61986729fe8e51b79d20b4ab8b9af2c197f360
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-04 23:18:54 +0530

    Finals Chnages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "res.cloudinary.com",
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+            },
         ],
+
     }
 }
 
